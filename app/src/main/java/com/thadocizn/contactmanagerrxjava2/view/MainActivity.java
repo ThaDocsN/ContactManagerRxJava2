a0c8acb9b49de1658df19342e58c67400b4b40fd
ThaDocIzN <rodan72@msn.com>
2019-06-10 12:31:39 -0500

    add contact class

--- app/src/main/java/com/thadocizn/contactmanagerrxjava2/view/MainActivity.java
+++ app/src/main/java/com/thadocizn/contactmanagerrxjava2/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.thadocizn.contactmanagerrxjava2;
+package com.thadocizn.contactmanagerrxjava2.view;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.thadocizn.contactmanagerrxjava2.R;
 
 public class MainActivity extends AppCompatActivity {
 
